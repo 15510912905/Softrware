3c1592979ceac15c0c5e1574f30886a840167477
arris9 <arris9@163.com>
2021-02-07 21:13:52 +0800

    First updata

---
+++ 必备软件/IDM 6.33.2/internet_download_manager_6.33.2/Тихая установка.cmd
@@ -0,0 +1,23 @@
+@ECHO OFF
+Color 0A
+@cls
+echo.
+echo.
+echo.
+@echo    ษอออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออป
+@echo    บ                                                                         บ
+@echo    บ                    ๋ฏฎซญ๏ฅโแ๏ ใแโ ญฎขช  ฏเฎฃเ ฌฌ๋...                   บ
+@echo    บ                                                                         บ
+@echo    บ                ฎฆ ซใฉแโ , คฎฆคจโฅแ์ ง ขฅเ่ฅญจ๏ ฎฏฅเ ๆจจ                บ
+@echo    บ                                                                         บ
+@echo    บ                                                                         บ
+@echo    บ                                                                         บ
+@echo    บ                                                                         บ
+@echo    บ  ฎแฅโจโฅ  แ ฉโ ฏเฎฅชโ  LREPACKS.RU, คซ๏ ง ฃเใงชจ คเใฃจๅ เฅฏ ชฎข   บ
+@echo    บ                                                                         บ
+@echo    บ                    http://lrepacks.ru/  - ฆค๑โ  แ!!!                   บ
+@echo    บ                                                                         บ
+@echo    ศอออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออผ
+@echo off
+FOR %%i IN ("Internet Download Manager*.exe") DO Set FileName="%%i"
+%FileName% /SILENT
